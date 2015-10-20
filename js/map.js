09973c5beeb28783ce946885d1df768fc941c742
Yvonne Ingles <ydingles@hotmail.com>
2015-10-19 23:59:12 -0700

    more changes

--- js/map.js
+++ js/map.js
@@ -3,9 +3,9 @@ var drawMap = function() {
 
   // Create map and set view
   var map = L.map('map', {
-    center: [47.6080, -122.3410],
-    zoom: 13,
-//  layers: [cities]
+ //   center: [47.6080, -122.3410],
+    center: [39, -98],
+    zoom: 4,
   });
 
   // Create a tile layer variable using the appropriate url
@@ -39,91 +39,90 @@ var getData = function(map) {
 // Loop through your data and add the appropriate layers and points
 var customBuild = function(map, data) {
   // Be sure to add each layer to the map
+  var hit = new L.LayerGroup([]).addTo(map);
+  var kill = new L.LayerGroup([]).addTo(map);
+  var nonwhite = new L.LayerGroup([]).addTo(map);
+  var white = new L.LayerGroup([]).addTo(map);
 
-  //var master = [].addTo(map);
+  var countnwm = 0;
+  var countnww = 0;
+  var countwm = 0;
+  var countww = 0;
 
-
-  var hokLayer = new L.layerGroup([]).addTo(map);
+ //var hokLayer = new L.layerGroup([]).addTo(map);
 
  // console.log(data);
  // console.log(jQuery.type(data));
 
   data.forEach(function(a) {
-    var City = a["City"];
-    var Age = a["Victim's Age"];
-    var Gender = a["Victim's Gender"];
-    var Race = a["Race"];
+    var name = a["Victim's Name"];
+    var age = a["Victim's Age"];
+    var gen = a["Victim's Gender"];
+    var race = a["Race"];
     var HoK = a["Hit or Killed?"];
+    var sum = a["Summary"];
     var lat = a["lat"];
     var lng = a["lng"];
   //  console.log(a);
   //  console.log(a["Race"]);
 
-  //var hokLayer;
-
     if (HoK == 'Hit') {
-      var circle = L.circle([lat, lng], 13, {
+      var circle = L.circle([lat, lng], 3, {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5
-      }).addTo(hokLayer);
+      }).bindPopup(sum).addTo(hit);
     } else {
-      var circle = L.circle([lat, lng], 13, {
+      var circle = L.circle([lat, lng], 3, {
         color: 'blue',
         fillColor: '#0000FF',
         fillOpacity: 0.5
-    }).addTo(hokLayer);
+    }).bindPopup(sum).addTo(kill);
 
     }
 
+    if (race != "White") {
+      var circle = L.circle([lat, lng], 3, {
+        color: 'brown',
+        fillColor: '#A52A2A',
+        fillOpacity: 0.5
+      }).bindPopup(sum).addTo(nonwhite);
+    } else {
+      var circle = L.circle([lat, lng], 3, {
+        color: 'yellow',
+        fillColor: '#FFFF00',
+        fillOpacity: 0.5
+      }).bindPopup(sum).addTo(white);
+
+    }
+
+    if (race == "White" && gen == "Male") {
+      var wm = countwm++;
+    } else if (race == "White" && gen == "Female") {
+      var ww = countww++;
+    } else if (race != "White" && gen == "Male") {
+      var nwm = countnwm++;
+    } else if (race != "White" && gen == "Female") {
+      var nww = countnww++
+    }
+
+
   });
 
-
-      // test test
-  // var cities = new L.LayerGroup();
-  // L.marker([47.6097,-122.3331]).bindPopup('This is Seattle').addTo(cities);
-  // cities.addTo(map);
-
-
-
-
-  // var overlays = {
-  //   "Cities": cities
-  // };
+  var layers = {
+    "Killed": kill,
+    "Hit": hit,
+    "Nonwhite": nonwhite,
+    "White": white
+  };
 
 
   // // Once layers are on the map, add a leaflet controller that shows/hides layers
-  //L.control.layers(null, hokLayer).addTo(map);
+  L.control.layers(null, layers).addTo(map);
+
+    // jquery to pass info to html table
+  $('#wm').text(countwm);
+  $('#ww').text(countww);
+  $('#nwm').text(countnwm);
+  $('#nww').text(countnww);
 }
-
-
-
-// tabulation
-
-//$("<tr></tr>").text(...)
-
-// var data = [1,2,3];
-// var obj1 = {
-//   person:'james',
-//   snack: 'gummies'
-// };
-
-// var obj2 = {
-//   person:'julian',
-//   snack: 'cheese-its'
-// };
-
-// var obj3 = {
-//   person: 'yvonne',
-//   snack: 'cheetos'
-// };
-
-// var myArr = [obj1, obj2, obj3];
-
-// myArr.forEach(function(d){
-//   console.log() //get things to pop up in console
-// })
-
-//forEach 
-
-
